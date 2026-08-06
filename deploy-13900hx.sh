34e028cd66197989b61ce4f5be29db4adb6e8b11
Narongchai Thongkongkeaw <dotnetnat@gmail.com>
2026-08-06 08:47:30 +0700

    add build and deploy

---
+++ deploy-13900hx.sh
@@ -0,0 +1,7 @@
+
+./scripts/deploy-mgmt-lab.sh --mgmt almalinux@10.1.1.150 \
+ --cp-gb 50 --worker-gb 75  --version 0.1.23
+
+scp out/pertisk-cloud-amd64{,-50g,-75g}.qcow2 almalinux@10.1.1.150:/tmp/
+ssh almalinux@10.1.1.150 'sudo mv /tmp/pertisk-cloud-amd64*.qcow2 /var/lib/pertisk-mgmt/images/ && sudo chown -R pertisk-mgmt:pertisk-mgmt /var/lib/pertisk-mgmt/images'
+#scp out/rpm/pertisk-mgmt-0.1.22-1.x86_64.rpm almalinux@10.1.1.150:/tmp
